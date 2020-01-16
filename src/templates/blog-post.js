c3f87eb16c6a2179232e2b61e203023a116fc504
Nicolas Carlo <nicolascarlo.espeon@gmail.com>
2020-01-16 01:34:38 -0500

    Remove dates from posts

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -22,16 +22,6 @@ class BlogPostTemplate extends React.Component {
           description={post.frontmatter.description || post.excerpt}
         />
         <h1>{post.frontmatter.title}</h1>
-        <p
-          style={{
-            ...scale(-1 / 5),
-            display: `block`,
-            marginBottom: rhythm(1),
-            marginTop: rhythm(-1),
-          }}
-        >
-          {post.frontmatter.date}
-        </p>
         <MDXRenderer>{post.body}</MDXRenderer>
         <CTA />
         <hr
