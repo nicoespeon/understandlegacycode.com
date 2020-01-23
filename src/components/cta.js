f1af2457390a9ccfcd5c5a50adcfa43c772502a2
Nicolas Carlo <nicolascarlo.espeon@gmail.com>
2020-01-23 00:09:56 -0500

    Add CTA to the blog

--- src/components/cta.js
+++ src/components/cta.js
@@ -1,7 +1,122 @@
 import React from "react"
+import styled from "styled-components"
+
+import { colors } from "../utils/typography"
 
 function CTA() {
-  return <></>
+  const botsHoneypot = (
+    <div style={{ position: "absolute", left: "-5000px" }} aria-hidden="true">
+      <input
+        type="text"
+        name="b_ca05d369ee1b4a4e803dce32c_2fd39e1501"
+        tabIndex="-1"
+        defaultValue=""
+      />
+    </div>
+  )
+
+  return (
+    <Wrapper>
+      <Form
+        action="https://nicoespeon.us4.list-manage.com/subscribe/post?u=ca05d369ee1b4a4e803dce32c&amp;id=2fd39e1501"
+        method="post"
+        name="mc-embedded-subscribe-form"
+        target="_blank"
+      >
+        <h2>
+          Learn to <em>understand</em> Legacy Code
+        </h2>
+        <p>
+          Every week I share <strong>practical tips</strong> to help people work
+          with Legacy Code.
+          <br />
+          Drop your email below and receive my advices for FREE.
+        </p>
+        <Container>
+          <Input type="name" placeholder="First name" name="FNAME" />
+          <Input
+            type="email"
+            placeholder="Email address"
+            name="EMAIL"
+            required
+          />
+          {botsHoneypot}
+          <Button type="submit">Subscribe 📨</Button>
+        </Container>
+        <p>I won't spam you. I won't share your email. Unsubscribe anytime.</p>
+      </Form>
+    </Wrapper>
+  )
 }
+
+const rotation = "1.5deg"
+
+const Wrapper = styled.div`
+  background-color: ${colors.background};
+  background-image: url("data:image/svg+xml,%3Csvg width='52' height='26' viewBox='0 0 52 26' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='none' fill-rule='evenodd'%3E%3Cg fill='%239C92AC' fill-opacity='0.2'%3E%3Cpath d='M10 10c0-2.21-1.79-4-4-4-3.314 0-6-2.686-6-6h2c0 2.21 1.79 4 4 4 3.314 0 6 2.686 6 6 0 2.21 1.79 4 4 4 3.314 0 6 2.686 6 6 0 2.21 1.79 4 4 4v2c-3.314 0-6-2.686-6-6 0-2.21-1.79-4-4-4-3.314 0-6-2.686-6-6zm25.464-1.95l8.486 8.486-1.414 1.414-8.486-8.486 1.414-1.414z' /%3E%3C/g%3E%3C/g%3E%3C/svg%3E");
+
+  transform: rotate(-${rotation});
+  padding: 5px 15px;
+  margin-bottom: 2rem;
+`
+
+const Form = styled.form`
+  transform: rotate(${rotation});
+`
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 1.75rem;
+
+  @media (min-width: 600px) {
+    flex-direction: row;
+    flex-wrap: nowrap;
+  }
+`
+
+const Input = styled.input`
+  padding: 10px 20px;
+  margin: 0 0 10px 0;
+
+  @media (min-width: 600px) {
+    margin: 0 10px 0 0;
+
+    &[type="name"] {
+      width: 25%;
+    }
+
+    &[type="email"] {
+      width: 45%;
+    }
+  }
+`
+
+const Button = styled.button`
+  padding: 15px 50px;
+  align-self: center;
+  color: white;
+  font-weight: 700;
+  background: ${colors.primary};
+  border: none;
+  box-shadow: inset 0 1px 0 hsl(280, 100%, 90%), 0 1px 3px hsla(0, 0%, 0%, 0.2);
+
+  &:hover {
+    cursor: pointer;
+    background: hsl(280, 100%, 75%);
+  }
+
+  &:active {
+    background: hsl(280, 90%, 60%);
+    border-color: hsl(280, 90%, 60%);
+    box-shadow: 0 2px 0 hsla(0, 0%, 100%, 0.15),
+      inset 0 2px 2px hsla(0, 0%, 0%, 0.1);
+  }
+
+  @media (min-width: 600px) {
+    width: 30%;
+    padding: 15px 20px;
+  }
+`
 
 export default CTA
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -21,11 +21,11 @@ class BlogPostTemplate extends React.Component {
           title={post.frontmatter.title}
           description={post.frontmatter.description || post.excerpt}
         />
-        <Wrapper>
-          <h1>{post.frontmatter.title}</h1>
+        <Title>{post.frontmatter.title}</Title>
+        <Content>
           <MDXRenderer>{post.body}</MDXRenderer>
-          <CTA />
-        </Wrapper>
+        </Content>
+        <CTA />
         <hr
           style={{
             marginBottom: rhythm(1),
@@ -62,16 +62,34 @@ class BlogPostTemplate extends React.Component {
   }
 }
 
-const Wrapper = styled.div`
+const Title = styled.h1`
+  font-weight: 300;
+  letter-spacing: -0.03em;
+  font-size: 2.5rem;
+
+  @media (min-width: 600px) {
+    font-size: 3.5rem;
+  }
+
+  @media (min-width: 1100px) {
+    position: relative;
+
+    &::after {
+      content: "";
+      position: absolute;
+      left: -3em;
+      width: calc(100% + 3em);
+      height: 0.3em;
+      bottom: 0;
+      background: ${colors.background};
+    }
+  }
+`
+
+const Content = styled.div`
   blockquote > p {
     font-size: 1.5em;
     line-height: 1.3em;
-  }
-
-  h1 {
-    font-weight: 300;
-    letter-spacing: -0.03em;
-    font-size: 2.5rem;
   }
 
   h3 {
