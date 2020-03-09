662c536593aecc4a459b49313816f3dbac817d99
Nicolas Carlo <nicolascarlo.espeon@gmail.com>
2020-03-09 12:50:31 -0400

    Update home page SEO title

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,7 +18,8 @@ class IndexPage extends React.Component {
       <Layout location={this.props.location} description={siteDescription}>
         <SEO
           date="2020-01-13"
-          title={siteDescription}
+          title="Understand Legacy Code"
+          description={siteDescription}
           keywords={[
             "legacy code",
             "technical debt",
