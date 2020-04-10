cdf0808491cec803d9d44bb61c81df0f7160d2cf
Nicolas Carlo <nicolascarlo.espeon@gmail.com>
2020-04-10 15:00:04 -0400

    Extract variables

--- src/pages/index.js
+++ src/pages/index.js
@@ -28,67 +28,51 @@ class IndexPage extends React.Component {
           ]}
         />
         <CTA />
-        <h2>Helpful Guides</h2>
-        <ul style={{ margin: "20px 0 40px" }}>
+        <h2>📖 Helpful Guides</h2>
+        <Ul>
           <Li key="getting-into-large-codebase">
-            <Link
-              style={{ fontSize: "21px" }}
-              to={`getting-into-large-codebase`}
-            >
+            <LargeLink to={`getting-into-large-codebase`}>
               Getting into a large codebase
-            </Link>
+            </LargeLink>
             <p>
               Diving into a large, undocumented codebase is overwhelming. Let's
               see some techniques to approach it.
             </p>
           </Li>
           <Li key="best-practice-or-code-smell">
-            <Link
-              style={{ fontSize: "21px" }}
-              to={`best-practice-or-code-smell`}
-            >
+            <LargeLink to={`best-practice-or-code-smell`}>
               Is it a best practice or a code smell?
-            </Link>
+            </LargeLink>
             <p>
               Not sure if a pattern will make the code more maintainable? Here
               are a few resources that will help.
             </p>
           </Li>
           <Li key="approval-tests">
-            <Link style={{ fontSize: "21px" }} to={`approval-tests`}>
-              Approval Testing
-            </Link>
+            <LargeLink to={`approval-tests`}>Approval Testing</LargeLink>
             <p>
               Techniques to quickly put Legacy Code under tests, so you can
               refactor it safely.
             </p>
           </Li>
           <Li key="code-feels-impossible-to-maintain">
-            <Link
-              style={{ fontSize: "21px" }}
-              to={`code-feels-impossible-to-maintain`}
-            >
+            <LargeLink to={`code-feels-impossible-to-maintain`}>
               Code feels impossible to maintain
-            </Link>
+            </LargeLink>
             <p>
               Sometimes, you seem to hit a point of no return and that the best
               strategy would be to burn it all and start over. Are there
               alternatives?
             </p>
           </Li>
-        </ul>
-        <h2>Latest articles</h2>
-        <ul style={{ margin: "20px 0 40px" }}>
+        </Ul>
+        <h2>💡 Latest articles</h2>
+        <Ul>
           {posts.map(({ node }) => {
             const title = node.frontmatter.title || node.fields.slug
             return (
               <Li key={node.fields.slug}>
-                <Link
-                  style={{ fontSize: "21px" }}
-                  to={`blog${node.fields.slug}`}
-                >
-                  {title}
-                </Link>
+                <LargeLink to={`blog${node.fields.slug}`}>{title}</LargeLink>
                 <p
                   dangerouslySetInnerHTML={{
                     __html: node.frontmatter.description || node.excerpt,
@@ -97,7 +81,7 @@ class IndexPage extends React.Component {
               </Li>
             )
           })}
-        </ul>
+        </Ul>
         <hr
           style={{
             marginBottom: rhythm(1),
@@ -114,6 +98,8 @@ const Li = styled.li`
     top: 16px;
   }
 `
+
+const LargeLink = props => <Link style={{ fontSize: "21px" }} {...props} />
 
 export default IndexPage
 
